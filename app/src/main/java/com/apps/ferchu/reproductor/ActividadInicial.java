53d778be65a2746895646dc496fd90edc93c1146
Fernando Luján Martínez <8flm95@gmail.com>
2018-03-12 02:05:38 +0100

    commit con un proyecto vacio, meto ahora lo del anterior

---
+++ app/src/main/java/com/apps/ferchu/reproductor/ActividadInicial.java
@@ -0,0 +1,13 @@
+package com.apps.ferchu.reproductor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActividadInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_inicial);
+    }
+}
